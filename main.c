e66cb3eb73864bcec290550a606f1b8ef9445822
blancojames88 <blancojames88@gmail.com>
2022-12-07 11:13:34 +0800

    testing vscode

--- main.c
+++ main.c
@@ -4,6 +4,11 @@ int main() {
     // This is a comment
     printf("Hello World!");
     printf("James' branch!")
+    int i=0;
+    
+    for (int i=0; i<=5; i++){
+        printf(i++);
+    }
 
     return 0;
 }
